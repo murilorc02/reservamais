08a5632af20cc6408097e41d78f932d6cab1d072
Murilo Ribeiro <murilorc02@gmail.com>
2024-11-16 19:58:18 -0300

    HTML base das páginas

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { HorariosAlunoComponent } from './components/horarios-aluno/horarios-aluno.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent],
-  template: `<app-login></app-login>`, // Carrega o LoginComponent
+  imports: [LoginComponent, HeaderComponent, HomeComponent, HorariosAlunoComponent],
+  template: `
+  <app-header/>
+  <!-- <app-login/> -->
+  <app-home/>
+  <app-horarios-aluno/>
+  `, // Carrega o LoginComponent
 })
 export class AppComponent {}
